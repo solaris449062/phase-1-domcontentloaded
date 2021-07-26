e8d9711b4ccf4eaae1c319401dc248702bc7a3a8
Sungoh Park <spark449062@gmail.com>
2021-07-25 23:56:10 -0600

    commit tree 9711d269f38c2a67a0fc31a769e5f5c30fba6f1d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    document.getElementById("text").textContent = "This is really cool!";
+    // console.log(targetText);
+});
+
+// console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+
+// if you want to use 'load' instead of 'DOMContentLoaded', you would need to change
+// from document.addEventListener() to window.addEventListener()
